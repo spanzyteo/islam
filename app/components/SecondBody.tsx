18c298de6ad8c7b1465957049c4b703b9abe3873
spanzy teo <spanzytheo@gmail.com>
2024-09-10 16:57:28 +0100

    recent modifications

--- app/components/SecondBody.tsx
+++ app/components/SecondBody.tsx
@@ -10,7 +10,7 @@ const SecondBody = () => {
         </h1>
       </div>
       <div className="mt-20 flex bg-[#305473] pb-20">
-        <div className="bg-[#375B7D] flex flex-col w-[30%] border-r border-r-[#F29F05]">
+        <div className="bg-[#375B7D] flex flex-col w-[33%] border-r border-r-[#F29F05]">
           <div className="ml-1 lg:ml-20 flex flex-col mt-24 ">
             <div className="text-2xl lg:text-4xl text-white font-bold">
               <h1>Our impact in</h1>
@@ -30,7 +30,7 @@ const SecondBody = () => {
             </div>
           </div>
         </div>
-        <div className="w-[70%] flex lg:flex-row flex-col lg:ml-10 ml-2 mt-20">
+        <div className="w-[64%] flex lg:flex-row flex-col lg:ml-10 ml-2 mt-20">
           <div className=" flex flex-col">
             <div>
               <h1
@@ -42,7 +42,7 @@ const SecondBody = () => {
               <h1 className="text-[#F29F05] text-2xl font-semibold mt-2">
                 emergency projects
               </h1>
-              <h1 className="text-white text-xl mt-4 lg:w-[100%] w-[250px]">
+              <h1 className="text-white md:text-xl text-lg mt-4 lg:w-[100%] w-[250px]">
                 reached 8.7 million people in 29 countries
               </h1>
             </div>
@@ -56,7 +56,7 @@ const SecondBody = () => {
               <h1 className="text-[#F29F05] text-2xl font-semibold mt-2">
                 major appeals
               </h1>
-              <h1 className="text-white text-xl mt-4 lg:w-[450px] w-[250px]">
+              <h1 className="text-white md:text-xl text-lg mt-4 lg:w-[450px] w-[250px]">
                 launched, and 3 maintained, to fund emergency responses
               </h1>
             </div>
@@ -67,13 +67,14 @@ const SecondBody = () => {
                 className="text-6xl text-transparent"
                 style={{ WebkitTextStroke: '2px #F29F05' }}
               >
-                326
+                10.2
               </h1>
               <h1 className="text-[#F29F05] text-2xl font-semibold mt-2">
-                emergency projects
+                million individuals
               </h1>
-              <h1 className="text-white text-xl mt-4 lg:w-[100%] w-[250px]">
-                reached 8.7 million people in 29 countries
+              <h1 className="text-white md:text-xl text-lg mt-4 lg:w-[100%] w-[250px]">
+                received life-saving food aid, including over 2 million monthly
+                in Yemen
               </h1>
             </div>
             <div className="mt-20">
@@ -81,13 +82,14 @@ const SecondBody = () => {
                 className="text-6xl text-transparent"
                 style={{ WebkitTextStroke: '2px #F29F05' }}
               >
-                6
+                132.6
               </h1>
               <h1 className="text-[#F29F05] text-2xl font-semibold mt-2">
-                major appeals
+                132.6 million
               </h1>
-              <h1 className="text-white text-xl mt-4 lg:w-[450px] w-[250px]">
-                launched, and 3 maintained, to fund emergency responses
+              <h1 className="text-white md:text-xl text-lg mt-4 lg:w-[450px] w-[250px]">
+                spent supporting people affected by and recovering from
+                disasters
               </h1>
             </div>
           </div>
