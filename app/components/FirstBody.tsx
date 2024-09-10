6ccf1477513d9c4d16909e0a4c5407bb2c016ac8
spanzy teo <spanzytheo@gmail.com>
2024-09-10 14:15:58 +0100

    recent modifications

--- app/components/FirstBody.tsx
+++ app/components/FirstBody.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const FirstBody = () => {
   return (
     <>
-      <div className="mt-16 md:w-[80%] w-[90%] mx-auto">
+      <div className="mt-16 md:w-[80%] w-[90%] mx-auto mb-40">
         <div className="flex items-center gap-4">
           <div className="md:h-[100px] h-[50px] w-[20px] bg-[rgb(48,81,118)]"></div>
           <div className="text-[#305176] lg:text-4xl text-3xl font-bold">
@@ -68,7 +68,7 @@ const FirstBody = () => {
             </div>
             <div className="h-[149px] w-[90%] lg:w-[514px] bg-white mt-[-5rem] shadow-md flex flex-col items-start pl-5">
               <div className="text-[#FFAE00] lg:mt-4 mt-2">News</div>
-              <div className="text-[#305176] lg:w-[450px] w-[350px] font-bold mt-0 lg:mt-2 text-xl">
+              <div className="text-[#305176] lg:w-[450px] w-[95%] font-bold mt-0 lg:mt-2 text-xl">
                 Millions affected as floods continue to sweep across Bangladesh
               </div>
               <button className="lg:mt-2 mt-0 text-gray-500 font-bold text-lg hover:opacity-90 hover:underline">
